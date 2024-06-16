c91b23a85ef63af0b775abff540e5858e6ce46d1
SachinKukkar <sachinkukkar1998@gmail.com>
2024-06-16 18:33:37 +0530

    Chapter 01 done

---
+++ Python_with_Harry/Chapter-01/first.py
@@ -0,0 +1,4 @@
+print("Hello World !")
+
+# Module - a file containing code which was written by somebody else , which can be imported and can be used in our programs \
+    # A Python module is like a toolbox filled with special tools (codes)
---
+++ Python_with_Harry/Chapter-01/module.py
@@ -0,0 +1,23 @@
+import pyjokes
+
+# print("Printing jokes ...")
+
+# This Prints a random joke 
+
+joke = pyjokes.get_joke()
+
+print(joke)
+
+
+## PIP is the python package manager which helps in installation of modules on our system
+
+# We have two types of Modules 
+# Built-in Modules and External Modules 
+
+
+# by writing Python in CMD , we can use python as calculator because it opens REPL (Read Print Evaluate Loop)
+
+
+"""
+We can have multiline comments using this 
+"""
