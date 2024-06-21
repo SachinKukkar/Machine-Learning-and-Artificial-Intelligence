2bab3da647fc9c31a49961cda6aa026843ea8595
SachinKukkar <sachinkukkar1998@gmail.com>
2024-06-21 10:00:21 +0530

    Chapter 05 Practice Set Done

---
+++ Python_with_Harry/Chapter-05-Practice-Set/Problem1.py
@@ -0,0 +1,12 @@
+# python program to create a dictionary of hindi words with values as their english translation  , provide the user an option to look up into it
+words = {
+    "madad" : "help",
+    "kursi" : "chair",
+    "billi" : "cat",
+    "kutta" : "dog" , 
+    "ladki" : "girl"
+}
+
+word = input("Enter the word you want to look up ")
+
+print(words[word])
---
+++ Python_with_Harry/Chapter-05-Practice-Set/Problem4.py
@@ -0,0 +1,30 @@
+# friends  = {
+#     "Sachin" : "C++"
+#     "Modani" : "Java"
+#     "Sanidhya" : "Python"
+#     "Bhumika" : "Assembly Language"
+# }
+
+
+d = {}
+name = input("Enter the friend name : ")
+lang = input("Enter the language : ")
+
+d.update({name:lang})
+
+name = input("Enter the friend name : ")
+lang = input("Enter the language : ")
+
+d.update({name:lang})
+
+name = input("Enter the friend name : ")
+lang = input("Enter the language : ")
+
+d.update({name:lang})
+
+name = input("Enter the friend name : ")
+lang = input("Enter the language : ")
+
+d.update({name:lang})
+
+print(d)
